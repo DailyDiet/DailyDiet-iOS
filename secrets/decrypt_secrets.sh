ace7131520b8c9f70d37f0be7c48edae50e4c49b
Alireza <mr.toghyiani@icloud.com>
2020-06-02 01:46:19 +0430

    fix profile 2

--- secrets/decrypt_secrets.sh
+++ secrets/decrypt_secrets.sh
@@ -6,14 +6,14 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./s
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+cp ./secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
 
-security create-keychain -p "$KEYCHAIN_PASS" build.keychain
-security import ./secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$KEYCHAIN_PASS" -A
+security create-keychain -p "1377Alireza1377" build.keychain
+security import ./secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "1377Alireza1377" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "$KEYCHAIN_PASS" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "1377Alireza1377" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "$KEYCHAIN_PASS" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "1377Alireza1377" ~/Library/Keychains/build.keychain
