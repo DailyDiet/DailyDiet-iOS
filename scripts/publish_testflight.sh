c176d2209062bf6edcee0af2bde0eb0536623175
Alireza <mr.toghyiani@icloud.com>
2020-06-02 01:40:07 +0430

    tmp

--- scripts/publish_testflight.sh
+++ scripts/publish_testflight.sh
@@ -3,4 +3,4 @@
 
 set -eo pipefail
 
-xcrun altool --upload-app -t ios -f build/DailyDiet -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
+
--- secrets/decrypt_secrets.sh
+++ secrets/decrypt_secrets.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./secrets/Certificates.p12 ./secrets/Certificates.p12.gpg
-
-
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./secrets/DailyDiet_Dist_profile.mobileprovision ./secrets/DailyDiet_Dist_profile.mobileprovision.gpg
-
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./secrets/DailyDiet_Dist_profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/DailyDiet_Dist_profile.mobileprovision
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
 
-security create-keychain -p "1377Alireza1377" build.keychain
-security import ./secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "1377Alireza1377" -A
+security create-keychain -p "$KEYCHAIN_PASS" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$KEYCHAIN_PASS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "1377Alireza1377" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "$KEYCHAIN_PASS" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "1377Alireza1377" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "$KEYCHAIN_PASS" ~/Library/Keychains/build.keychain
