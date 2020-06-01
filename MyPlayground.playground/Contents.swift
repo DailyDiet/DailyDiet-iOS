c3e5b7802abf1d8b0c4824733aa568444344c04e
Alireza <mr.toghyiani@icloud.com>
2020-06-01 19:48:51 +0430

    tmp

--- MyPlayground.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-import sw
-
-var str = "Hello, playground"
-
-var string = "{\"id\": 412923, \"category\": \"breakfast           \", \"image\": \"https://images.eatthismuch.com/site_media/img/412923_simmyras_ddc29085-a105-4a41-b99c-66c4d070afab.png\", \"thumbnail\": \"https://images.eatthismuch.com/site_media/thmb/412923_simmyras_ddc29085-a105-4a41-b99c-66c4d070afab.png\", \"title\": \"Greek Yogurt and Berry Parfait\", \"nutrition\": {\"calories\": 344, \"fat\": 10.6, \"fiber\": 5.5, \"protein\": 29.2}}"
-
-
