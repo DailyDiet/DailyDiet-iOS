ad6c892affa47db2edc280eb2fbcdb63330ed2ce
Alireza <mr.toghyiani@icloud.com>
2020-06-02 00:17:24 +0430

    fixing archive

--- scripts/archive_app.sh
+++ scripts/archive_app.sh
@@ -2,10 +2,17 @@
 #!/bin/bash
 
 set -eo pipefail
+#
+#xcodebuild -workspace ./DailyDiet.xcworkspace \
+#            -scheme DailyDiet.xcscheme
+#            -sdk iphoneos \
+#            -configuration AppStoreDistribution \
+#            -archivePath $PWD/build/DailyDiet.xcarchive \
+#            clean archive | xcpretty
 
-xcodebuild -workspace ./DailyDiet.xcworkspace \
-            -scheme DailyDiet.xcscheme
-            -sdk iphoneos \
-            -configuration AppStoreDistribution \
-            -archivePath $PWD/build/DailyDiet.xcarchive \
-            clean archive | xcpretty
+xcodebuild -sdk iphoneos -project DailyDiet/DailyDiet.xcworkspace \
+-configuration Release -scheme DailyDiet \
+-derivedDataPath DerivedData \
+-archivePath $PWD/build/DailyDiet.xcarchive \
+clean archive | xcpretty
+
