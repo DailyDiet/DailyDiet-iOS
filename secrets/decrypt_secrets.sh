fcbcc6afe6c26f49b2ccb7656e6059de93ed97e8
Alireza <mr.toghyiani@icloud.com>
2020-06-01 22:45:22 +0430

    add pass to keychain

--- secrets/decrypt_secrets.sh
+++ secrets/decrypt_secrets.sh
@@ -12,11 +12,11 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./secrets/DailyDiet_Dist_profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/DailyDiet_Dist_profile.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-security import ./secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "$IOS_KEYS" build.keychain
+security import ./secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "$IOS_KEYS" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "$IOS_KEYS" ~/Library/Keychains/build.keychain
